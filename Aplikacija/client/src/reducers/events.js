053963b60496bf9c3dc7d311a62c390c99599044
ntasicc <nikols.tt@elfak.rs>
2021-04-05 18:07:55 +0200

    Reducer - fetch and create implemented

---
+++ Aplikacija/client/src/reducers/events.js
@@ -0,0 +1,13 @@
+import { FETCH_ALL, CREATE } from '../constants/actionTypes'
+
+export default (events = [], action) => {
+  switch (action.type) {
+    case FETCH_ALL:
+      return action.payload
+    case CREATE:
+      return [...events, action.payload]
+
+    default:
+      return events
+  }
+}
