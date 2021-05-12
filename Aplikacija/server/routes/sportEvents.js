feac7542a837863aa1bf75d05d7b7f2f21bc2d15
ntasicc <nikols.tt@elfak.rs>
2021-05-12 19:57:36 +0200

    added auth middleware to router

--- Aplikacija/server/routes/sportEvents.js
+++ Aplikacija/server/routes/sportEvents.js
@@ -1,17 +1,21 @@
-import express, { Router } from 'express';
-import {getSportEvents, createSportEvent, updateSportEvent, getSportEventById} from '../controllers/sportEventsController.js';
+import express, { Router } from 'express'
+import {
+  getSportEvents,
+  createSportEvent,
+  updateSportEvent,
+  getSportEventById,
+} from '../controllers/sportEventsController.js'
 
-import auth from '../middleware/auth.js';
+import auth from '../middleware/auth.js'
 
-
-const router= express.Router();
+const router = express.Router()
 
 /* ISPRED RUTA DODATI auth, za one rute kojima treba autentifikacija,
 npr za get()Events rute ne treba jer Evente moze videti svako */
 /*potrebno dodati deleteEvent*/
-router.get('/', getSportEvents);
-router.get('/:id', getSportEventById);
-router.post('/add', createSportEvent); 
-router.patch('/:id', updateSportEvent);
+router.get('/', getSportEvents)
+router.get('/:id', getSportEventById)
+router.post('/add', auth, createSportEvent)
+router.patch('/:id', auth, updateSportEvent)
 
-export default router;
+export default router
