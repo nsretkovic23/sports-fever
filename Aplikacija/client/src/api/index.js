4c126e6eb5db0de3d9903f4fa90e314c247e6746
ntasicc <nikols.tt@elfak.rs>
2021-05-12 19:55:11 +0200

    signin and signout are now post methods (error corrected)

--- Aplikacija/client/src/api/index.js
+++ Aplikacija/client/src/api/index.js
@@ -2,7 +2,16 @@ import axios from 'axios'
 
 const API = axios.create({ baseURL: 'http://localhost:5000' })
 
+API.interceptors.request.use((req) => {
+  if (localStorage.getItem('profile')) {
+    req.headers.Authorization = `Bearer ${
+      JSON.parse(localStorage.getItem('profile')).token
+    }`
+  }
+
+  return req
+})
 export const fetchEvents = () => API.get('/event')
 export const createEvent = (newEvent) => API.post(`/add`, newEvent)
-export const signIn = (data) => API.get('/user/signin', data)
-export const signUp = (data) => API.get('/user/signup', data)
+export const signIn = (data) => API.post('/user/signin', data)
+export const signUp = (data) => API.post('/user/signup', data)
