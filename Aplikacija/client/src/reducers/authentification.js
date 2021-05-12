5a18a70be84c27de069d8313221917de232be81c
ntasicc <nikols.tt@elfak.rs>
2021-05-12 19:55:53 +0200

    typo corrected

--- Aplikacija/client/src/reducers/authentification.js
+++ Aplikacija/client/src/reducers/authentification.js
@@ -5,7 +5,7 @@ const authReducer = (state = { authData: null }, action) => {
     case actionType.AUTH:
       localStorage.setItem('profile', JSON.stringify({ ...action?.data }))
 
-      return { ...state, authData: action.data }
+      return { ...state, authData: action?.data }
     case actionType.LOGOUT:
       localStorage.clear()
 
