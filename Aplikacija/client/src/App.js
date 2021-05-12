46d91a2137bd793f4f52a6f8fe5c96e7fadcecc7
ntasicc <nikols.tt@elfak.rs>
2021-05-12 19:57:55 +0200

    Navbar and Router added

--- Aplikacija/client/src/App.js
+++ Aplikacija/client/src/App.js
@@ -2,14 +2,24 @@ import React from 'react'
 
 import { FindEventPage } from './components/Find Event/FindEventPage'
 import { CreateEvent } from './components/Create Event/CreateEvent'
+import { Nav } from './components/Navigation/Nav'
+import { Authentification } from './components/Authentification/Authentification'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 
 const App = () => {
   return (
     <>
-      <div>
-        <FindEventPage />
-        <CreateEvent />
-      </div>
+      <Router>
+        <Nav />
+        <Switch>
+          <Route exact path='/' component={FindEventPage}></Route>
+          <Route
+            path='/authentification'
+            exact
+            component={Authentification}
+          ></Route>
+        </Switch>
+      </Router>
     </>
   )
 }
