1137fae7f1ff3a675381e2fae850b988cdda1f6e
ntasicc <nikols.tt@elfak.rs>
2021-05-17 13:04:59 +0200

    Added update and delete functionalities, form component as well

--- Aplikacija/client/src/components/Find Event/Event.js
+++ Aplikacija/client/src/components/Find Event/Event.js
@@ -1,24 +1,79 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
-import { useLocation } from 'react-router-dom'
-import { getEvent } from '../../actions/event'
+import { useLocation, useHistory } from 'react-router-dom'
+import { getEvent, deleteEvent, updateEvent } from '../../actions/event'
+import { Form } from '../Create Event/Form'
+
 export const Event = () => {
   const user = JSON.parse(localStorage.getItem('profile'))
   const location = useLocation()
   const { _id } = location.state
   const event = useSelector((state) => state.events)
+  const [update, setUpdate] = useState(null)
+  const [refresh, setRefresh] = useState(null)
+  const [newEvent, setNewEvent] = useState({
+    title: '',
+    description: '',
+    date: '',
+    free_spots: '',
+    sport: '',
+  })
   const dispatch = useDispatch()
+  const history = useHistory()
+
+  useEffect(() => {
+    if (event) setNewEvent(event)
+  }, [event])
 
   useEffect(() => {
     dispatch(getEvent(_id))
-  }, [_id, dispatch])
+    console.log('fetching')
+  }, [_id, dispatch, refresh])
 
+  const handleChange = (e) => {
+    e.preventDefault()
+    const name = e.target.name
+    const value = e.target.value
+    setNewEvent({ ...newEvent, [name]: value })
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    if (newEvent.title && newEvent.description && newEvent.free_spots > 0) {
+      console.log(newEvent)
+      dispatch(updateEvent(event._id, newEvent))
+      setUpdate(null)
+      setRefresh((prev) => !prev)
+    } else {
+      alert('Wrong inputs')
+    }
+  }
+
+  console.log(newEvent)
   return (
     <>
       <div>
         {user?.result?.googleId === event.creator ||
         user?.result?._id === event.creator ? (
-          <p>Its your event, you can change it</p>
+          <div>
+            <button
+              onClick={(ev) => {
+                ev.preventDefault()
+                setUpdate(true)
+              }}
+            >
+              Update
+            </button>
+            <button
+              onClick={(ev) => {
+                ev.preventDefault()
+                dispatch(deleteEvent(_id))
+                history.push('/')
+              }}
+            >
+              Delete
+            </button>
+          </div>
         ) : null}
       </div>
       <div>
@@ -28,6 +83,17 @@ export const Event = () => {
         <p>{event?.sport}</p>
         <p>{event?.free_spots}</p>
       </div>
+      {update ? (
+        <div>
+          <Form
+            event={newEvent}
+            handleChange={handleChange}
+            handleSubmit={handleSubmit}
+            longitude={newEvent.longitude}
+            latitude={newEvent.latitude}
+          />
+        </div>
+      ) : null}
     </>
   )
 }
