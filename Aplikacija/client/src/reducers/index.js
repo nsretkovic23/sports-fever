37f606bbcdf570fcadd522d9df111f2edb25f385
ntasicc <nikols.tt@elfak.rs>
2021-06-19 20:24:46 +0200

    report and credit reducer added

--- Aplikacija/client/src/reducers/index.js
+++ Aplikacija/client/src/reducers/index.js
@@ -1,6 +1,7 @@
 import { combineReducers } from 'redux'
 import auth from './authentification'
 import events from './events'
-import admin from './admin'
+import reports from './reports'
+import credits from './credits'
 
-export const reducers = combineReducers({ events, auth, admin })
+export const reducers = combineReducers({ events, auth, reports, credits })
