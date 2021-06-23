15441c3df16bf7691aaa3f59a9def8a63e0c4269
Nikola <nsretkovic@elfak.rs>
2021-06-23 18:52:23 +0200

    isAdmin and isBanned properties added

--- Aplikacija/server/models/user.model.js
+++ Aplikacija/server/models/user.model.js
@@ -1,6 +1,8 @@
 import mongoose from 'mongoose';
 
 const userSchema = mongoose.Schema({
+    isAdmin:Boolean,
+    isBanned:Boolean,
     name: {type: String, required:true},
     email: {type:String, required:true},
     password: {type:String, required:true},
