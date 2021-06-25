5781dd11773265d0ee516bb78187bedeea7ec9dd
ntasicc <nikols.tt@elfak.rs>
2021-06-25 22:34:05 +0200

    Map with single event

---
+++ Aplikacija/client/src/components/Maps/MapWithSingleEvent.js
@@ -0,0 +1,61 @@
+import React, { useRef, useCallback } from 'react'
+import { Search, Locate } from './MapFunctions'
+import { GoogleMap, useLoadScript, Marker } from '@react-google-maps/api'
+
+import { libraries, mapContainerStyle, center } from './MapConst'
+import '@reach/combobox/styles.css'
+import useStyles from './styles'
+
+export const MapWithSingleEvent = ({ event }) => {
+  const classes = useStyles()
+  const { isLoaded, loadError } = useLoadScript({
+    googleMapsApiKey: 'AIzaSyCEbMl51eshDuU7zH8SFgkkuTbd4AjGeys',
+    libraries,
+  })
+  const mapRef = useRef()
+
+  const onMapLoad = useCallback((map) => {
+    mapRef.current = map
+  }, [])
+
+  const panTo = useCallback(({ lat, lng }) => {
+    mapRef.current.panTo({ lat, lng })
+    mapRef.current.setZoom(15)
+  }, [])
+
+  if (loadError) return 'Error loading map'
+  if (!isLoaded) return 'Loading Map'
+
+  return (
+    <div className={classes.map}>
+      <Search panTo={panTo} />
+      <Locate panTo={panTo} />
+      <GoogleMap
+        id='map'
+        mapContainerStyle={mapContainerStyle}
+        zoom={14}
+        center={center}
+        onLoad={onMapLoad}
+      >
+        {event ? drawMarker(event) : null}
+      </GoogleMap>
+    </div>
+  )
+}
+
+function drawMarker(marker) {
+  return (
+    <Marker
+      key={`${marker.lat}-${marker.lng}`}
+      position={{
+        lat: marker.lat,
+        lng: marker.lng,
+      }}
+      icon={{
+        url: `/${marker.sport}.png`,
+        origin: new window.google.maps.Point(0, 0),
+        scaledSize: new window.google.maps.Size(80, 80),
+      }}
+    />
+  )
+}
